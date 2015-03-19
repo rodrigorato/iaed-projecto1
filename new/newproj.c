c61851f6a022909ec9a0e8fc8a9f1b933fee218c
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2015-03-19 18:12:40 +0000

    talvez um dia

--- new/newproj.c
+++ new/newproj.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAXBANCOS 1000
+#define MAXNOME 41
+
+typedef struct BANKSTRUCT{
+	char nome[MAXNOME];
+	int ref;
+	int class;
+	int parcs;
+}banco;
+
+int matriz[MAXBANCOS][MAXBANCOS];
+banco listabancos[MAXBANCOS];
+int q
+
 int main(){
+
 
 }
